603fe38f71486972c09a02678a4b484523c55c14
zhangzheng <18601254750@163.com>
2017-04-12 10:10:15 +0000

    添加系统路径

--- kong/init.sh
+++ kong/init.sh
@@ -7,6 +7,8 @@ echo "pg_user = postgres" >> /etc/kong/kong.conf
 echo "pg_password = postgres" >> /etc/kong/kong.conf
 echo "pg_database = postgres" >> /etc/kong/kong.conf
 
+echo 'PATH="/usr/local/openresty/bin/:"$PATH' >> /etc/profile
+
 localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
 
 exit 0
