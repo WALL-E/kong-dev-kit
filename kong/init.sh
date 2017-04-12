389f95619050e18b8f1fa9008db67196b311646b
zhangzheng <18601254750@163.com>
2017-04-12 09:32:59 +0000

    初始化字符集

--- kong/init.sh
+++ kong/init.sh
@@ -6,3 +6,7 @@ echo "pg_host = postgres" >> /etc/kong/kong.conf
 echo "pg_user = postgres" >> /etc/kong/kong.conf
 echo "pg_password = postgres" >> /etc/kong/kong.conf
 echo "pg_database = postgres" >> /etc/kong/kong.conf
+
+localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+
+exit 0
