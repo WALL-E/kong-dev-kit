c6699b20cf1855f0c90bcd3668842bbeaaa57583
zhangzheng <zzgigi2003@163.com>
2017-08-08 09:42:50 +0800

    update init.sh

--- kong/init.sh
+++ kong/init.sh
@@ -13,4 +13,11 @@ echo 'LANG="zh_CN.UTF-8"'  > /etc/locale.conf
 localedef -v -c -i zh_CN -f UTF-8 zh_CN.UTF-8
 localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
 
+cd ~
+git clone https://github.com/WALL-E/kong.git
+cd kong/
+git checkout -b feat/cors origin/feat/cors
+make dev
+make lint
+
 exit 0
